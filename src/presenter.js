590f60ae39d0c0b019dc964c97d0de8c3088eed4
Elian <eliantito234@gmail.com>
2023-02-16 00:54:39 -0400

    Multiplicacion agregada we :v

--- src/presenter.js
+++ src/presenter.js
@@ -1,9 +1,15 @@
 import sumar from "./sumador";
+import multiplicar from "./multiplicador";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
+const first2 = document.querySelector("#primer-numero2");
+const second2 = document.querySelector("#segundo-numero2");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
+const div2 = document.querySelector("#resultado-div2");
+const form2 = document.querySelector("#multiplicar-form");
+
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -12,4 +18,13 @@ form.addEventListener("submit", (event) => {
   const secondNumber = Number.parseInt(second.value);
 
   div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first2.value);
+  const secondNumber = Number.parseInt(second2.value);
+
+  div2.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
 });
